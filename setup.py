b41235b72e885d2d9df0a025c726e2703a1b21b0
Marco Guerreiro <mguerreiro@lrs.eit.uni-kl.de>
2020-12-10 12:38:55 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pydsim',
+      version='0.1.0',
+      description='Python discrete simulation',
+      #url='https://github.com/mtguerreiro/pynoise.git',
+      author='Marco Guerreiro',
+      author_email='marcotulio.guerreiro@gmail.com',
+      packages=['pydsim'],
+      )
