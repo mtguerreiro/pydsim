6d21b8858f91978dbe127c77ffa32bf423a7629b
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2021-03-01 12:56:39 +0100

    initial pegam module

--- pydsim/__init__.py
+++ pydsim/__init__.py
@@ -2,4 +2,5 @@ from . import control
 from . import nbutils
 from . import pe
 from . import peode
+from . import pegam
 from . import utils
---
+++ pydsim/pegam.py
@@ -0,0 +1,205 @@
+import time
+import numpy as np
+import scipy
+import numba
+
+import pydsim.peode as peode
+import pydsim.utils as pydutils
+import pydsim.control as pydctl
+import pydsim.nbutils as pydnb
+
+import pysp
+import pynoise
+
+
+def buck_f(t, x, A, B, u):
+    return A @ x + B * u
+
+class Buck:
+
+    def __init__(self, R, L, C):
+
+        self.R = R
+        self.L = L
+        self.C = C
+
+        self.max_step = 1e-6
+        self.init_params()
+
+        # Continuous model
+        self.Am = np.array([[0,     -1/L],
+                            [1/C,   -1/R/C]])
+        self.Bm = np.array([[1/L],
+                            [0]])
+        self.Cm = np.array([0, 1])
+
+        self.x_ini = np.zeros((1, 2))
+
+    
+    def init_params(self):
+        self.t_pwm = None
+        self.t_sim = None
+        self.v_in = None
+
+
+    def set_pwm(self, t_pwm, n_pwm=None):
+        self.t_pwm = t_pwm
+
+    def set_max_step(self, max_step):
+        self.max_step = max_step
+
+    def set_sim_time(self, t_sim):
+        self.t_sim = t_sim
+
+    
+##    def set_run_params(self):
+##        self.n = round(self.t_sim / self.dt)
+##        self.n_cycles = round(self.t_sim / self.t_pwm)
+##        self.t = self.dt * np.arange(self.n)
+##        self.set_vectors()
+##        print('\n---------------------------------------------')
+##        print('|{:^43}|'.format('Run params'))
+##        print('---------------------------------------------')
+##        print('|{:^21}|{:^21.4e}|'.format('Max. step size', self.max_step))
+##        print('|{:^21}|{:^21}|'.format('Sim cycles', self.n_cycles))
+##        print('|{:^21}|{:^21}|'.format('State points', self.n))
+##        print('---------------------------------------------\n')
+
+
+    def set_v_in(self, v_in):
+        self.v_in = v_in
+
+
+    def set_initial_conditions(self, il, vc):
+        self.x_ini[0, 0] = il
+        self.x_ini[0, 1] = vc
+
+
+    def set_ctlparams(self, params):
+        self.ctlparams = params
+
+
+    def gam_matrices(self, k, f):
+        w = 2 * np.pi * f
+        R = self.R; L = self.L; C = self.C; v_in = self.v_in
+        
+        A = np.array([[0,       k*w,    -1/L,       0],
+                      [-k*w,    0,      0,          -1/L],
+                      [1/C,     0,      -1/(R*C),   k*w],
+                      [0,       1/C,    -w*k,       -1/(R*C)]])
+        
+        B = np.array([[v_in / (2*np.pi*k*L)],
+                      [v_in / (2*np.pi*k*L)],
+                      [0],
+                      [0]])
+
+        C = np.array([[np.sin(2*np.pi*k*0.5)],
+                      [np.cos(2*np.pi*k*0.5) - 1],
+                      [0],
+                      [0]])
+
+        return A, B, C
+        
+##    def set_vectors(self):
+##        self.t = self.dt * np.arange(self.n)
+##        self.x = np.zeros((self.n, 2))
+##        self.u = np.zeros((self.n, 1))
+##        self.pwm = np.zeros((self.n, 1))
+##        self.e = np.zeros((self.n, 1))
+    
+    
+    def sim(self, v_ref, v_in=None, control='ol', k=None):
+
+        # Loads/creates useful variables
+        t_pwm = self.t_pwm
+        max_step = self.max_step
+        t_sim = self.t_sim
+        k = [1]
+
+        if v_in is None:
+            if self.v_in is None:
+                print('Need to define v_in before simulating!')
+                return -1
+            v_in = self.v_in
+        
+
+##        # Sets v_ref and v_in as vectors if they are numbers
+##        if type(v_ref) is int or type(v_ref) is float:
+##            v_ref = v_ref * np.ones(n_cycles)
+##
+##        if type(v_in) is int or type(v_in) is float:
+##            v_in = v_in * np.ones(n_cycles)
+##        elif v_in is None:
+##            v_in = self.v_in * np.ones(n_cycles)
+
+##        # Control
+##        if control == 'pi':
+##            ctlparams = self.ctlparams
+##            ctlparams['dt'] = t_pwm
+##            ctlini = {'e_1': v_ref[0] - x[0, 1], 'u_1': v_ref[0] / v_in[0]}
+##            ctl = pydctl.PI(ctlparams)
+##            #ctl.set_initial_conditions(ctlini)
+##
+##        elif control == 'pid':
+##            ctlparams = self.ctlparams
+##            ctlparams['dt'] = t_pwm
+##            ctlini = {'e_1': v_ref[0] - x[0, 1], 'u_1': v_ref[0] / v_in[0]}
+##            ctl = pydctl.PID(ctlparams)
+##            if self.x_ini[0, 0] != 0 and self.x_ini[0, 1] != 0:
+##                ctl.set_initial_conditions(u_1=v_ref[0] / v_in[0], u_2=v_ref[0] / v_in[0])
+##        
+##        elif control == 'mpc':
+##            ctlparams = self.ctlparams
+##            ctlparams['A'] = self.Am
+##            ctlparams['B'] = self.Bm
+##            ctlparams['C'] = self.Cm
+##            ctlparams['dt'] = t_pwm
+##            ctl = pydctl.MPC(ctlparams)
+##            self.ctl = ctl
+##
+##        else:
+##            ctlparams = {'dc': v_ref[0] / v_in[0]}
+##            ctlini = {'dc': v_ref[0] / v_in[0]}
+##            ctl = pydctl.OL(ctlparams)
+##            ctl.set_initial_conditions(ctlini)
+    
+        _ti = time.time()
+        #print(self.x_ini)
+        x0 = (self.x_ini[0, 0], self.x_ini[0, 1])
+
+        # Starts by solving the dc component
+        Adc = self.Am
+        Bdc = self.Bm
+
+        def f_x_dc(t, x):
+            x_dot = Adc @ x + Bdc * 0.5 * v_in
+            return x_dot
+
+        sol_dc = scipy.integrate.solve_ivp(f_x_dc, [0, t_sim], x0, max_step=max_step, vectorized=True)
+        t = sol_dc.t
+
+        for ki in k:
+            print(ki)
+            Ah, Bh, Ch = self.gam_matrices(ki, 1 / t_pwm)
+            x0 = [0, 0, 0, 0]
+            def f_h(t, x):
+                x_dot = Ah @ x + Bh * Ch
+                return x_dot
+            sol_h = scipy.integrate.solve_ivp(f_h, [0, t_sim], x0, t_eval=t, max_step=max_step, vectorized=True)
+
+            i_l_ki_re = sol_h.y[0, :]
+            i_l_ki_im = sol_h.y[1, :]
+            i_l_ki = 2 * (i_l_ki_re * np.cos(ki*2*np.pi/t_pwm*t) - i_l_ki_im * np.sin(ki*2*np.pi/t_pwm*t))
+
+            v_c_ki_re = sol_h.y[2, :]
+            v_c_ki_im = sol_h.y[3, :]
+            v_c_ki = 2 * (v_c_ki_re * np.cos(ki*2*np.pi/t_pwm*t) - v_c_ki_im * np.sin(ki*2*np.pi/t_pwm*t))                        
+                
+        _tf = time.time()
+        print('Sim time: {:.4f} s\n'.format(_tf - _ti))
+
+        self.t = t
+        self.x = np.zeros((t.shape[0], 2))
+        self.x[:, 0] = sol_dc.y[0, :] + i_l_ki
+        #self.il = sol_dc[0, :] + i_l_ki
+        self.x[:, 1] = sol_dc.y[1, :] + v_c_ki
