a6b67c0a82a02245fec561211629abef39cbf28d
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2021-04-21 10:01:07 +0200

    Changed obs example

--- examples/buck_sfb_obs.py
+++ examples/buck_sfb_obs.py
@@ -24,21 +24,21 @@ f_pwm = 200e3
 dt = 1 / f_pwm / 100
 
 # Specs for state feedback
-Ts = 5e-3
-os = 5/100
+Ts = 1.5e-3
+os = 1/100
 
 # --- Simulation ---
 buck = pyd.peode.Buck(R, L, C)
 buck.set_f_pwm(f_pwm)
 buck.set_sim_params(dt, t_sim)
-buck.set_initial_conditions(1, 1)
+#buck.set_initial_conditions(1, 1)
 
 n = round(t_sim * f_pwm)
 v_in_p = v_in * np.ones(n)
-#v_in_p[n>>1:] = v_in + v_in_step
+v_in_p[n>>1:] = v_in + v_in_step
 
 v_ref_p = v_ref * np.ones(n)
-v_ref_p[n>>1:] = v_ref + v_in_step
+#v_ref_p[n>>1:] = v_ref + v_in_step
 
 # Plant poles
 zeta = -np.log(os) / np.sqrt(np.pi**2 + (np.log(os))**2)
@@ -48,9 +48,12 @@ p2 = np.conj(p1)
 p3 = 10 * p1.real
 
 # Observer poles
-p1_o = 30 * p1
+##p1_o = 30 * p1
+##p2_o = np.conj(p1_o)
+##p3_o = 10 * p1_o.real
+p1_o = 10 * p1
 p2_o = np.conj(p1_o)
-p3_o = 10 * p1_o.real
+p3_o = 5 * p1_o.real
 
 # Sim
 sfb_params = {'poles': [p1, p2, p3], 'poles_o': [p1_o, p2_o, p3_o]}
