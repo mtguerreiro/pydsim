4324dc6ef2f6e0bcc63b1ff376ef7e96f546b5b0
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2021-04-22 17:58:22 +0200

    Example of boost with sfb

--- examples/boost_sfb.py
+++ examples/boost_sfb.py
@@ -11,11 +11,11 @@ C = 560e-6
 
 # Input and reference voltage
 v_in = 10
-v_in_step = -3
+v_in_step = 1
 v_ref = 15
 
 # Sim time
-t_sim = 3e-3
+t_sim = 5e-3
 
 # PWM frequency
 f_pwm = 200e3
@@ -31,11 +31,11 @@ os = 10/100
 buck = pyd.peode.Boost(R, L, C)
 buck.set_f_pwm(f_pwm)
 buck.set_sim_params(dt, t_sim)
-buck.set_initial_conditions(0, 0)
+buck.set_initial_conditions(2, 11)
 
 n = round(t_sim * f_pwm)
 v_in_p = v_in * np.ones(n)
-#v_in_p[n>>1:] = v_in + v_in_step
+v_in_p[n>>1:] = v_in + v_in_step
 
 zeta = -np.log(os) / np.sqrt(np.pi**2 + (np.log(os))**2)
 wn = 4/Ts/zeta
@@ -50,13 +50,6 @@ buck.sim(v_ref=v_ref, v_in=v_in_p, controller=pyd.control.LinSFB)
 t_sfb = buck.signals.t
 x_sfb = buck.signals.x
 u_sfb = buck.signals.d
-
-##buck.set_f_pwm(f_pwm / 2)
-##buck.sim(v_ref=v_ref, v_in=v_in, controller=pyd.control.SFB)
-##t_sfb1 = buck.signals.t
-##x_sfb1 = buck.signals.x
-##u_sfb1 = buck.signals.d
-
 
 # --- Results ---
 plt.figure(figsize=(10,6))
